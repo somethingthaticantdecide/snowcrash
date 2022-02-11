028414d0fc9fe0754a2d383e6b916b13a0fb88bd
somethingthaticantdecide <somethingthaticantdecide@gmail.com>
2022-02-11 23:58:17 +0300

    levle13

--- level13/Ressources/do.sh
+++ level13/Ressources/do.sh
@@ -1,5 +1,14 @@
-echo '#include<unistd.h>uid_t getuid(void){ return 4242;}' > getuid.c
+# echo '#include<unistd.h>uid_t getuid(void){ return 4242;}' > getuid.c
+# gcc -fPIC -shared -o lib.so getuid.c
+# export LD_PRELOAD="$PWD/lib.so"
 
-gcc -fPIC -shared -o lib.so getuid.c
-
-export LD_PRELOAD="$PWD/lib.so"
+///
+gdb level13
+r
+break getuid
+r
+step
+i r
+set $eax = 4242
+i r
+c
