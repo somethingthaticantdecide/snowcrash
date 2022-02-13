d7e756b4fcac8af50a214d610fdaf0a8f7b2ddb2
somethingthaticantdecide <somethingthaticantdecide@gmail.com>
2022-02-13 13:23:08 +0300

    next

--- level02/Ressources/do.sh
+++ level02/Ressources/do.sh
@@ -1,2 +1,4 @@
 scp -P 4242 level02@192.168.52.128:/home/user/level02/level02.pcap  C:\git\exam06
-tcpick -C -yP -r level02.pcap
+https://bikulov.org/blog/2012/11/03/tools-for-tracing-a-pcap-file-in-linux-bash/
+https://linux.die.net/man/8/tcpick
+tcpick -yP -r level02.pcap
